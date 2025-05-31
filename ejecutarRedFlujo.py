09ea4ebfb269c9b4422e277c9e658ead45ae7936
Udistefano <udistefano@fi.uba.ar>
2025-05-31 18:55:27 -0300

    agrego time en red de flujo y actualizo set de datos

--- ejecutarRedFlujo.py
+++ ejecutarRedFlujo.py
@@ -1,4 +1,5 @@
 import ejRedFlujo
+import time
 
 def leerDatasetDesdeArchivo(nombre_archivo):
     with open(nombre_archivo, 'r') as archivo:
@@ -15,10 +16,13 @@ def leerDatasetDesdeArchivo(nombre_archivo):
 
     return distancias, D, k, b, n
 
-nombre_archivo = "casos_red_de_Flujo/matriz6.txt"
+nombre_archivo = "casos_red_de_Flujo/matriz4.txt"
 distancias, D, k, b, n = leerDatasetDesdeArchivo(nombre_archivo)
-
+inicio = time.time()
 resultado = ejRedFlujo.asignarBackupsAntenas(distancias, D, k, b, n)
+fin = time.time()
+tiempo_total = fin - inicio
+
 print("Conjuntos de backups por antena:")
 if isinstance(resultado, str):
     print(resultado)
@@ -26,3 +30,5 @@ else:
     for i in range(len(resultado)):
         backup = resultado[i]
         print("Antena " + str(i+1) + ": " + "tiene como backup la antena " + str(backup))
+
+print(f"Tiempo de ejecución: {tiempo_total:.4f} segundos")
